ff5f33ae443b61cb221dc1ce871a72c7db3fcbec
Mike <mikedstark@users.noreply.github.com>
2016-09-11 06:31:30 -0600

    Code cleanup

--- pong.rb
+++ pong.rb
@@ -1,14 +1,9 @@
 require 'gosu'
 
-
-
 class Pong < Gosu::Window
 	def initialize width=800, height=600, fullscreen=true
-
 		super
 		self.caption = 'Pong'
-		
-
 		@score1 = 0
 		@score2 = 0
 		@paddle_length = 100
@@ -21,34 +16,24 @@ class Pong < Gosu::Window
 		@font1 = Gosu::Font.new(50)
 		@font2 = Gosu::Font.new(50)
 
-		@ball = Gosu::Image.new("_res/ball.png")
+		@ball = Gosu::Image.new("ball.png")
 		
 		@xBall = self.width/2 - @ball.width/2
 		@yBall = self.height/2 - @ball.height/2
 		@velocity_x = 3
 		@velocity_y = 3
-
-
-
 	end
 
 	def button_down id
-		
 		close if id == Gosu::KbEscape
-
 		#@score1 += 1 if id == Gosu::Kb5
 		#@score2 += 1 if id == Gosu::Kb6
-
-
 	end
 
 	def update
-
-
 		@xBall += @velocity_x	
 		@yBall += @velocity_y
 
-		
 		#p1 scores
 		if @xBall > self.width + @ball.width 
 			@velocity_x *= -1
@@ -70,8 +55,6 @@ class Pong < Gosu::Window
 			@velocity_y *= -1
 		end
 
-
-
 		#Bounce off P1 paddle
 		if 	@yBall >= @y - @ball.height && @yBall <= @y + @paddle_length  &&
 			@xBall < @distance_from_wall + @paddle_width/2 &&
@@ -85,13 +68,9 @@ class Pong < Gosu::Window
 		if 	@yBall >= @y2 - @ball.height && @yBall <= @y2 + @paddle_length  &&
 			@xBall + @ball.width > self.width - @distance_from_wall - @paddle_width/2 &&
 			@xBall + @ball.width < self.width - @distance_from_wall
-
 			@velocity_x *= -1
 			#@velocity_y *= -1
 		end
-
-
-
 
 		#paddle 1 going down
 		if button_down?(Gosu::KbS)
@@ -120,23 +99,14 @@ class Pong < Gosu::Window
 				@y2 -= 5
 			end
 		end
-
-
 	end
 
 	def draw
-
 		draw_net()
 		draw_player_1()
 		draw_player_2()
-
 		draw_score()
-
 		draw_ball()
-
-
-	
-
 	end
 
 	def draw_ball
@@ -144,38 +114,26 @@ class Pong < Gosu::Window
 	end
 
 	def draw_score
-
 		@font1.draw @score1.to_s, self.width/4, 10, 1
 		@font2.draw @score2.to_s, self.width/1.5, 10, 1
-		
 	end
 
-	
-
 	def draw_net
-
 		x = self.width/2
 		y1 = 10
 		y2 = 20
 		size = 5
 		dist_between = 30
 
-
 		for i in 0..19
-
 			draw_quad(x-size,y1 + i*dist_between,0xffffffff,
 					  x+size,y1 + i*dist_between,0xffffffff,
 					  x-size,y2 + i*dist_between,0xffffffff,
 					  x+size,y2 + i*dist_between,0xffffffff)
-
 		end
-		
 	end
 
 	def draw_player_1
-		
-
-
 		x1 = @distance_from_wall
 		x2 = x1 + @paddle_width
 		y1 = @y
@@ -187,8 +145,6 @@ class Pong < Gosu::Window
 	end
 
 	def draw_player_2
-
-
 		x1 = self.width - @distance_from_wall - @paddle_width
 		x2 = x1 + @paddle_width
 		y1 = @y2
@@ -197,8 +153,6 @@ class Pong < Gosu::Window
 				  x2,y1,0xffffffff,
 				  x1,y2,0xffffffff,
 				  x2,y2,0xffffffff)
-
-		
 	end
 
 end ##Pong
